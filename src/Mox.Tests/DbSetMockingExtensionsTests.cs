a1c08f91c8e97592ef0903cd53d97c3050082f27
Mikhail Filippov <mikhail@filippov.me>
2014-12-30 20:56:47 +0300

    Add MVC context request url mocking.

---
+++ src/Mox.Tests/DbSetMockingExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Moq;
+using Xunit;
+using Xunit.Should;
+
+namespace Mox.Tests
+{
+    public class Entity { }
+    
+    public class DbSetMockingExtensionsTests
+    {
+        [Fact]
+        public void ShouldMockDbSetData()
+        {
+            Mock.Of<DbSet<Entity>>().MockWithList(new List<Entity> { new Entity(), new Entity() }).ToList().Count.ShouldBe(2);
+        }
+    }
+}
---
+++ src/Mox.Tests/MvcControllerMockingExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+using Xunit;
+using Xunit.Should;
+
+namespace Mox.Tests
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "Film", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+
+    public class DemoController : Controller
+    {
+        public string GetTestString()
+        {
+            return "test";
+        }
+
+        public string GetActionUrl()
+        {
+            return Url.Action("GetActionUrl", "Demo", null, Request.Url.Scheme);
+        }
+    }
+
+    public static class MvcControllerMockingExtensions
+    {
+        public static T SetGetContextWithUrl<T>(this T controller, string url, Action<RouteCollection> setupRoutes) where T : Controller
+        {
+            var httpContext = Mock.Of<HttpContextBase>();
+            Mock.Get(httpContext).Setup(o => o.Request.Url).Returns(new Uri(url));
+            Mock.Get(httpContext).Setup(o => o.Response.ApplyAppPathModifier(It.IsAny<string>())).Returns<string>(r => r);
+
+            controller.ControllerContext = new ControllerContext(httpContext, new RouteData(), controller);
+            
+            var routes = new RouteCollection();
+            setupRoutes(routes);
+            
+            controller.Url = new UrlHelper(new RequestContext(httpContext, new RouteData()), routes);
+
+            return controller;
+        }
+
+    }
+
+    public class MvcControllerMockingExtensionsTests
+    {
+        [Fact]
+        public void ShouldInvokeSimpleMethodWithContext()
+        {
+            new DemoController().SetGetContextWithUrl("http://test.ru/test", RouteConfig.RegisterRoutes).GetTestString().ShouldBe("test");
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectActionUrlWithContext()
+        {
+            new DemoController().SetGetContextWithUrl("http://test.ru/test", RouteConfig.RegisterRoutes).GetActionUrl().ShouldBe("http://test.ru/Demo/GetActionUrl");
+        }
+    }
+}
