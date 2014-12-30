9160e02d42585a2a53d2468613ddbc4503e8d9fd
Mikhail Filippov <mikhail@filippov.me>
2014-12-30 12:06:16 +0300

    Add MockWithList method.

---
+++ src/Mox/DbSetMockingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Moq;
+
+namespace Mox
+{
+    public static class DbSetMockingExtensions
+    {
+        public static DbSet<T> MockWithList<T>(this DbSet<T> dbSet, IList<T> data) where T : class
+        {
+            var queryable = data.AsQueryable();
+            Mock.Get(dbSet).As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            Mock.Get(dbSet).As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            Mock.Get(dbSet).As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            Mock.Get(dbSet).As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
+
+            return dbSet;
+        }
+    }
+}
---
+++ src/Mox/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mox")]
+[assembly: AssemblyCopyright("Copyright © Mikhail Filippov 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cb66fc91-897f-4738-badf-8623bdedbb60")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
