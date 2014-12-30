86e53801959608d64419301d205a7587a3e213db
Mikhail Filippov <mikhail@filippov.me>
2014-12-30 14:11:19 +0300

    Add tests to MockWithLIst method.

---
+++ src/Mox.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mox.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mox.Tests")]
+[assembly: AssemblyCopyright("Copyright © Mikhail Filippov 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5cc0db9-45fb-4068-bfc5-3b39495efcc6")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
